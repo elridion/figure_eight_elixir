b3155ef96a7dd7beb70a792104b48cf9a2977740
Hans Bernhard Gödeke <hgoedeke@frobese.de>
2018-07-17 13:37:19 +0200

    initial commit

---
+++ lib/figure_eight_elixir.ex
@@ -0,0 +1,18 @@
+defmodule FigureEight do
+  @moduledoc """
+  Documentation for FigureEight.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FigureEight.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/figure_eight_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule FigureEightTest do
+  use ExUnit.Case
+  doctest FigureEight
+
+  test "greets the world" do
+    assert FigureEight.hello() == :world
+  end
+end
